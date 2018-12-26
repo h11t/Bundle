2e64b431f6f3e4c4d162e49b6b1de32a6ab3e0ae
h11t <8023562+h11t@users.noreply.github.com>
2018-12-26 16:22:31 +0300

    bundle operations and enable optimization

---
+++ Bundle_Islemleri/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Optimization;
+
+namespace Bundle_Islemleri.App_Start
+{
+    public class BundleConfig
+    {
+        internal static void RegisterBundle(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/jquery-1.10.2.js",
+                "~/Scripts/modernizr-2.6.2.js"
+                ));
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/Site.css"
+                ));
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
--- Bundle_Islemleri/Global.asax.cs
+++ Bundle_Islemleri/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Bundle_Islemleri.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace Bundle_Islemleri
@@ -13,6 +15,7 @@ namespace Bundle_Islemleri
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundle(BundleTable.Bundles);
         }
     }
 }
